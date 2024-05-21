3762482cbbc0aae163dfc60e381efec73b76d148
lucas_finoti <lucasfinotirodrigues@hotmail.com>
2024-05-21 16:41:15 -0300

    feat: service and get at pokeapi added

--- pokeAPI/src/app/home/home.component.ts
+++ pokeAPI/src/app/home/home.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+// home.component.ts
+import { Component, OnInit } from '@angular/core';
+import { PokeApiService } from 'src/services/pokeAPI.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  listaPokemons: any[] = [];
+  listaImagensPokemons: any;
+
+  private apiUrlImage = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+
+
+  constructor(private pokeApiService: PokeApiService) {}
+
+  ngOnInit(): void {
+    this.getPokemons();
+  }
+
+  getPokemons(): void {
+    this.pokeApiService.getPokemon().subscribe(response => {
+      this.listaPokemons = response.results.map((pokemon: any, index: number) => ({
+        ...pokemon,
+        id: index + 1
+      }));
+      console.warn("Lista de Pokémons => ", this.listaPokemons);
+    });
+  }
+
+  getPokemonImageUrl(id: number): string {
+    return `${this.apiUrlImage}/${id}.png`;
+  }
 }
---
+++ pokeAPI/src/services/pokeAPI.service.ts
@@ -0,0 +1,17 @@
+// pokeAPI.service.ts
+import { Injectable } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokeApiService {
+  private apiUrl = 'https://pokeapi.co/api/v2/pokemon?limit=150&offset=0';
+
+  constructor(private http: HttpClient) {}
+
+  getPokemon(): Observable<any> {
+    return this.http.get<any>(this.apiUrl);
+  }
+}
