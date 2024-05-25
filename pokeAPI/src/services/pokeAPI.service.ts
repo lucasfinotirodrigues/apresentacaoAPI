589483100ce2ec77052e0fa6edfc22257beffa73
lucas_finoti <lucasfinotirodrigues@hotmail.com>
2024-05-25 11:52:00 -0300

    feat: changed at service of API

--- pokeAPI/src/services/pokeAPI.service.ts
+++ pokeAPI/src/services/pokeAPI.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class PokeApiService {
-  private apiUrl = 'https://pokeapi.co/api/v2/pokemon?limit=150&offset=0';
+  private apiUrl = 'https://pokeapi.co/api/v2/pokemon?limit=151&offset=0';
 
   constructor(private http: HttpClient) {}
 
